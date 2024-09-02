a131fc430b5b668a4693f213cab62b7034d1e89c
Akshay Anand <21bcs015@iiitdmj.ac.in>
2024-09-02 23:06:34 +0530

    home route added

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to MongoDB:', err);
 });
 // Use routes
+app.get('/',(req,res)=>{
+    res.send({"Message":"Visit these URLs",
+        'URL1':`https://cryptotransactiontracer.onrender.com/api/expenses/0xce94e5621a5f7068253c42558c147480f38b5e0d`,
+         'URL2':`https://cryptotransactiontracer.onrender.com/api/transactions/0xce94e5621a5f7068253c42558c147480f38b5e0d`
+    })
+})
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/expenses', expenseRoutes);
 
